caa8513beb16738679578cd7ee6de605dfa1fe8c
broezer <broezer@gmail.com>
2017-03-23 11:20:01 +0100

    update rooms to temp

---
+++ templates/page-rooms-temp.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Template Name: Rooms (Temporay)
+ * The template for Rooms
+ *
+ */
+get_header(); ?>
+
+<main>
+  <?php get_template_part( 'templates/html/rooms', 'temp' );?>
+</main>
+
+<?php get_footer(); ?>
