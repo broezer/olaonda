a5620cbe471f7d74f1a4dbf34304460fa1fe73b9
broezer <broezer@gmail.com>
2017-03-23 14:44:52 +0100

    update GA code

--- footer.php
+++ footer.php
@@ -94,15 +94,13 @@
 <?php endif;?>
 
 <script>
-  //Google Analytics Code - Replace 'UA-XXXXXX-X'
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.nothing=g;m.parentNode.insertBefore(a,m)
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
-  ga('create', 'UA-XXXXXX-X', 'auto');
+  ga('create', 'UA-88922254-1', 'auto');
   ga('send', 'pageview');
-
 </script>
 
 </body>
