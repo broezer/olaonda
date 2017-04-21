d70eca9db5eb82e17c9b91ff0aac6974940c92af
broezer <broezer@gmail.com>
2017-04-21 11:05:04 +0100

    add contact page

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php
+get_header(); ?>
+
+<main class="page-default">
+	<article>
+		<header>
+			<h1 class="green">404 wave not found</h1>
+		</header>
+
+	</article>
+</main>
+
+
+<?php get_footer(); ?>
