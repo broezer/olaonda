0e445e73660401058234982f6b61dbff7401db65
broezer <broezer@gmail.com>
2017-05-10 14:24:31 +0200

    add area title

--- templates/parts/area-title.php
+++ templates/parts/area-title.php
@@ -5,6 +5,6 @@ $title = get_sub_field('title');
 ?>
 <article>
   <header>
-    <h1 class="yellow"><?php echo $title ?></h1>
+    <h1 class="green"><?php echo $title ?></h1>
   </header>
 </article>
