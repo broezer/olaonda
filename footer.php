266d75cc7dc62f4418906816e3399f349073d00a
broezer <broezer@gmail.com>
2017-05-19 16:53:58 +0200

    add credits

--- footer.php
+++ footer.php
@@ -73,6 +73,7 @@
        <?php wp_nav_menu( array( 'theme_location' => 'footer-menu', 'container' => '', 'items_wrap' => '%3$s') ); ?>
      </ul>
     </nav>
+    <h2 class="credits">code &amp; design -  <a href="https://brucemoerdjiman.com" target="_blank">Bruce Moerdjiman</a></h2>
     <?php endif;?>
   </section>
 </footer>
