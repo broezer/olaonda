8e277040e0c9d17cb6508207d8680ca002fefdbb
broezer <bruce@moerdjiman.com>
2019-10-23 23:53:40 +0200

    fix price notice and other stuff

--- templates/page-rooms-casa.php
+++ templates/page-rooms-casa.php
@@ -74,9 +74,7 @@ $image_url = $image['sizes'][ $size ];
     </section>
     <h2 class="special-notice"><?php the_field('message');?></h2>
     <section class="prices">
-    <p class="full-width">
-      <?php the_field('notice');?>
-    </p>
+    
     <?php 
         // check for rows (sub repeater)
         if( have_rows('prices') ): ?>
@@ -95,7 +93,11 @@ $image_url = $image['sizes'][ $size ];
         <?php endwhile; ?>
         </ul>
         <?php endif; //if( get_sub_field('items') ): ?>
+        
     </section>
+    <p class="full-width">
+      <?php the_field('notice');?>
+    </p>
   </article>
   
   <?php
@@ -131,7 +133,7 @@ $image_url = $image['sizes'][ $size ];
                 <g>
                     <rect id="Rectangle-2" stroke="#89DAC1" stroke-width="11" x="5.5" y="5.5" width="422" height="114"></rect>
                     <text id="" font-family="'Montserrat', sans-serif" font-size="24" font-weight="bold" letter-spacing="5.5999999" fill="#89DAC1">
-                        <tspan x="42" y="72">Book a room</tspan>
+                        <tspan x="42" y="72">Book Casa Olá Onda</tspan>
                     </text>
                     <polygon id="Triangle" fill="#89DAC1" points="374 63.5 349 76 349 51"></polygon>
                 </g>
