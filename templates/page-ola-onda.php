3a304d168f1956c7ad0aa5f78926ae7bcb54cc31
broezer <bruce@moerdjiman.com>
2020-02-23 22:27:50 +0100

    update paragraph font & update button

--- templates/page-ola-onda.php
+++ templates/page-ola-onda.php
@@ -187,7 +187,7 @@ $breakfast_img_url = $breakfast_img['sizes'][ $size_03 ];
               <defs></defs>
                 <g>
                     <rect id="Rectangle-2" stroke="#89DAC1" stroke-width="11" x="5.5" y="5.5" width="422" height="114"></rect>
-                    <text id="" font-family="Montserrat-Bold, Montserrat" font-size="24" font-weight="bold" letter-spacing="5.5999999" fill="#89DAC1">
+                    <text id="" font-family="Montserrat" font-size="24" font-weight="bold" letter-spacing="5.5999999" fill="#89DAC1">
                         <tspan x="42" y="72">View rooms</tspan>
                     </text>
                     <polygon id="Triangle" fill="#89DAC1" points="374 63.5 349 76 349 51"></polygon>
